c26ecca3dc9fc0ac3f2a95f73faaba26c3354f0c
Etty <beltrami.ester@gmail.com>
2021-12-29 21:25:34 +0100

    Try

---
+++ components/connector.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+export const Connector = ({ connector, width, height }) => {
+  const [x1, y1, x2, y2] = connector;
+
+  return (
+    <i
+      style={{
+        position: 'absolute',
+        width: Math.max(1, (x2 - x1) * width + 1),
+        height: Math.max(1, (y2 - y1) * height + 1),
+        background: `#999`,
+        transform: `translate(${x1 * width}px, ${y1 * height}px)`,
+        pointerEvents: 'none',
+      }}
+    />
+  );
+};
